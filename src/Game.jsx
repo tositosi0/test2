ee533ddd6948b09821f45c4d3e698cfd1e20282c
ToshiyaTokumasu <qwxftgb99@gmail.com>
2025-12-12 13:28:04 +0900

    Fix image rendering bug and implement camera system with larger world v1.0.8

--- src/Game.jsx
+++ src/Game.jsx
@@ -6,7 +6,7 @@ const GAME_HEIGHT = 640;
 const MAX_WEAPONS = 6;
 const MAX_PASSIVES = 6;
 const STAGE_DURATION = 60;
-const VERSION = 'v1.0.7';
+const VERSION = 'v1.0.8';
 const MAX_GEMS = 40; // 経験値アイテムの上限
 const MAX_PARTICLES = 30; // パーティクルの上限
 
